a282115d78b39a9764e20f3307378c8cc9589845
Neko Ayaka <neko@ayaka.moe>
2024-12-02 16:37:11 +0800

    chore: configure rules for static assets

--- constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
-
-export const llmInferenceEndToken = '<|llm_inference_end|>'
--- layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <main text="gray-700 dark:gray-200" h-full font-sans>
-    <slot />
-  </main>
-</template>
