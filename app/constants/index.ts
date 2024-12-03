cf1f1fe0380312e2ddec9082cfbea8e76ae198d1
Neko Ayaka <neko@ayaka.moe>
2024-12-03 13:06:39 +0800

    chore: cleanup
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- app/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const appName = 'アイリ VTuber'
-export const appDescription = 'アイリ VTuber - LLM Powered Live2D VTuber'
-
-export const llmInferenceEndToken = '<|llm_inference_end|>'
--- app/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <ClientOnly>
-      <!-- <MainStage /> -->
-    </ClientOnly>
-  </div>
-</template>
