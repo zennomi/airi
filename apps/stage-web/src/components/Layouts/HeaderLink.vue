b551c1be319255a1e39f39fd8dfd01fd9b91eb8f
junkwarrior87 <115852752+junkwarrior87@users.noreply.github.com>
2025-03-08 22:16:51 +0800

    feat(stage-web): make the logo also follow the color theme (#61)

--- apps/stage-web/src/components/Layouts/HeaderLink.vue
+++ apps/stage-web/src/components/Layouts/HeaderLink.vue
@@ -14,13 +14,19 @@ const dark = useDark()
     gap-2 px-2 text-nowrap text-2xl outline-none
   >
     <template v-if="dark">
-      <img :src="LogoDark" h-8 w-8>
+      <img :src="LogoDark" h-8 w-8 class="theme-colored">
     </template>
     <template v-else>
-      <img :src="Logo" h-8 w-8>
+      <img :src="Logo" h-8 w-8 class="theme-colored">
     </template>
     <div font-cute>
       <span>アイリ</span>
     </div>
   </RouterLink>
 </template>
+
+<style scoped>
+.theme-colored {
+  filter: hue-rotate(calc(var(--theme-colors-hue, 0) * 1deg));
+}
+</style>
