e62f8aa47e2528b842b6f57a72a308b3a1206705
Neko Ayaka <neko@ayaka.moe>
2025-04-13 18:30:06 +0800

    fix(vad): type

--- packages/stage-ui/src/components/Menu/index.ts
+++ packages/stage-ui/src/components/Menu/index.ts
@@ -1,2 +1,3 @@
+export { default as CharacterCard } from './CharacterCard.vue'
 export { default as IconItem } from './IconItem.vue'
 export { default as IconStatusItem } from './IconStatusItem.vue'
