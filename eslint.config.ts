5d994609eded2aae1b5698bb3e292f9ce148df76
Neko <neko@ayaka.moe>
2025-06-26 19:17:02 +0800

    Revert "perf(eslint): use moeru config & oxlint (#227)" (#231)
    This reverts commit 6d02cb0a016dc13b41712a9dd02b213c7f90c4ac.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,41 @@
-import { defineConfig } from '@moeru/eslint-config'
+import antfu from '@antfu/eslint-config'
 
-export default defineConfig({
-  oxlint: true,
-  // TODO: enable this
-  preferArrow: false,
-  preferLet: false,
-  typescript: true,
-  unocss: true,
-  vue: true,
-}, {
-  ignores: [
-    '**/assets/js/**',
-    '**/assets/live2d/models/**',
-    'apps/stage-tamagotchi/out/**',
-    'apps/stage-tamagotchi/src-tauri/**',
-    'crates/**',
-    '**/drizzle/**',
-    '**/.astro/**',
-  ],
-})
+export default await antfu(
+  {
+    unocss: true,
+    vue: true,
+    ignores: [
+      '**/assets/js/**',
+      '**/assets/live2d/models/**',
+      'apps/stage-tamagotchi/out/**',
+      'apps/stage-tamagotchi/src-tauri/**',
+      'crates/**',
+      '**/drizzle/**',
+      '**/.astro/**',
+    ],
+    rules: {
+      'import/order': 'off',
+      'antfu/import-dedupe': 'error',
+      'style/padding-line-between-statements': 'error',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          groups: [
+            'type-builtin',
+            'type-import',
+            'type-internal',
+            ['type-parent', 'type-sibling', 'type-index'],
+            'value-builtin',
+            'value-external',
+            'value-internal',
+            ['value-parent', 'value-sibling', 'value-index'],
+            ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
+            'side-effect',
+            'style',
+          ],
+          newlinesBetween: 'always',
+        },
+      ],
+    },
+  },
+)
