c00e11290ac53912e980c5fc5f7e3a00fbeaf01e
Neko Ayaka <neko@ayaka.moe>
2025-07-02 03:17:34 +0800

    feat(stage-ui): added LevelMeter component

--- packages/stage-ui/src/components/Gadgets/index.ts
+++ packages/stage-ui/src/components/Gadgets/index.ts
@@ -1,3 +1,4 @@
 export { default as AudioSpectrum } from './AudioSpectrum.vue'
 export { default as AudioSpectrumVisualizer } from './AudioSpectrumVisualizer.vue'
+export { default as LevelMeter } from './LevelMeter.vue'
 export { default as TestDummyMarker } from './TestDummyMarker.vue'
