db38c2270becb6c1785635fd8d97620ab44b5d8f
Neko Ayaka <neko@ayaka.moe>
2025-07-08 03:15:00 +0800

    fix(stage-web): nprogress disabled when PWA

--- apps/stage-web/src/utils/platform.ts
+++ apps/stage-web/src/utils/platform.ts
@@ -1,3 +1,18 @@
 export function isPlatformTamagotchi() {
   return import.meta.env.MODE === 'tamagotchi'
 }
+
+export function isStandalone() {
+  if (import.meta.env.SSR) {
+    return false
+  }
+  if (!('window' in globalThis && globalThis.window != null)) {
+    return false
+  }
+
+  const mediaStandalone = ('matchMedia' in window) && window.matchMedia('(display-mode: standalone)').matches
+  const navigatorStandalone = ('navigator' in window && 'standalone' in window.navigator && window.navigator.standalone)
+  const referrerStandalone = document.referrer.includes('android-app://')
+
+  return mediaStandalone || navigatorStandalone || referrerStandalone
+}
